3fd9bba0941e0c86a7631cbbe3ca51f997ef44b9
HFReasearches <hfresearches@gmail.com>
2026-06-16 22:48:06 +0000

    buffer latency control added in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
 #include <mutex>
 #include <thread>
+#include <chrono>
 
 #include "MarketData.hpp"
 
+using namespace std::chrono_suffix_literals;
 int main(){
   std::thread c(getCandles, "DXY");
   c.detach();
  
   while(true){
     std::lock_guard<std::mutex> lock(mtx);
+
+    size_t length = sizeof(period); 
+    if(length <= 0){
+      std::this_thread::sleep_for(20ms);
+    
+      std::cerr << "candle buffer empty\n";
+    }
+
     std::cout << period[sizeof(period)].close <<
     ":close\n";
   }
