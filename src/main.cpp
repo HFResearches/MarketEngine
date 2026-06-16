b87765c4d818ad673d1203427c206ca3a0847b91
HFReasearches <hfresearches@gmail.com>
2026-06-16 23:04:08 +0000

    it has been taking of period[index] because it was maybe doing output segmentation fault

--- src/main.cpp
+++ src/main.cpp
@@ -11,17 +11,7 @@ int main(){
   c.detach();
  
   while(true){
-    std::lock_guard<std::mutex> lock(mtx);
-
-    size_t length = sizeof(period); 
-    if(length <= 0){
-      std::this_thread::sleep_for(20ms);
-    
-      std::cerr << "candle buffer empty\n";
-    }
-
-    std::cout << period[sizeof(period)].close <<
-    ":close\n";
+    std::cout << "Running main.\n";
   }
 
   return 0;
