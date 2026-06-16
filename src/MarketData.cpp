646dac1c68c1940015d687a8b6f3d3471392f09a
HFReasearches <hfresearches@gmail.com>
2026-06-16 21:52:16 +0000

    now period in for container has method '.size()'

--- src/MarketData.cpp
+++ src/MarketData.cpp
@@ -73,7 +73,7 @@ void getCandles(const std::string symbol){
   while((bytes = SSL_read(ssl, buffermemoria,
     sizeof(buffermemoria) - 1)) > 0){
     std::lock_guard <std::mutex> lockar(mtx);
-    for(size_t x{0} : period){
+    for(size_t x{0} : period.size()){
       buffermemoria[bytes] = '\0';
       resposta += buffermemoria;   
       
@@ -84,7 +84,7 @@ void getCandles(const std::string symbol){
       period[x].close = j["close"].get<double>(); 
     }
     
-    for(size_t x : period)
+    for(size_t x : period.size())
       period[x] = {};
   }
 
