74a2579c6a5def766b29167d8df59d5cf745f7f1
HFResearches <hfresearches@gmail.com>
2026-07-07 14:38:30 -0300

    setted cxx 20 version

--- src/MarketData.hpp
+++ src/MarketData.hpp
@@ -13,12 +13,13 @@ struct alignas(32) candle{
 extern candle period[1024];
 extern std::mutex mtx;
 
-void getCandles(const std::string symbol);
+consteval void getCandles(const std::string
+symbol) noexcept;
 
 class OHLC{
   public:
-    inline double net(size_t x);
-    inline double body(size_t x);
-    inline double lowerShadow(size_t x);
-    inline double upperShadow(size_t x);
+    inline double net(size_t x) noexcept;
+    inline double body(size_t x) noexcept;
+    consteval double lowerShadow(size_t x);
+    consteval double upperShadow(size_t x);
 };
